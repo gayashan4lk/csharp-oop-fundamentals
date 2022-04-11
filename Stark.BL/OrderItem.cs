7ed518ef983bc2c72dc1d8f00825f049cb2c8511
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-11 12:04:17 +0530

    create oderItem class

---
+++ Stark.BL/OrderItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stark.BL
+{
+    public class OrderItem
+    {
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int orderItemId)
+        {
+            OrderItemIt = orderItemId;
+        }
+
+        public int OrderItemIt { get; private set; }
+        public int ProductId { get; set; }
+        public decimal? PurchasePrice { get; set; }
+        public int Quantity { get; set; }
+
+        public OrderItem Retrieve(int orderItemId)
+        {
+            return new OrderItem();
+        }
+
+        public bool Validate()
+        {
+            bool isValid = (Quantity <= 0 || ProductId <= 0 || PurchasePrice == null) ? false : true;
+            return isValid;
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+    }
+}
