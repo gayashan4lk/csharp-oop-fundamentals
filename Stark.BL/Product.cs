54a3d82c0f5f4be2fb77a2b09faf00694cd89b51
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-11 09:36:46 +0530

    create product class

---
+++ Stark.BL/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stark.BL
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        public Product(int productId)
+        {
+            ProductId = productId;
+        }
+
+        public int ProductId { get; private set; }
+        public string ProdcutName { get; set; }
+        public string Description { get; set; }
+        public double CurrentPrice { get; set; }
+
+        public bool Validate()
+        {
+            return true;
+        }
+
+        public Product Retrieve(int productId)
+        {
+            return new Product();
+        }
+
+
+        public List<Product> Retrieve()
+        {
+            return new List<Product>();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+    }
+}
