3e29fad1c3954e775343f9fc345658643def7f38
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-12 12:14:01 +0530

    create EntityBase Class

---
+++ Stark.BL/EntityBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stark.BL
+{
+    public enum EntityStateOption
+    {
+        Active,
+        Deleted
+    }
+
+    public abstract class EntityBase
+    {
+        public bool IsNew { get; private set; }
+        public bool HasChanges { get; set; }
+        public bool IsValid
+        {
+            get { return true; }
+        }
+        public EntityStateOption EntityState { get; set; }
+    }
+}
