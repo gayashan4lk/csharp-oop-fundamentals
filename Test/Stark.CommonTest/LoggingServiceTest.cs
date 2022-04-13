1bfdd16ccdb5fadd4fd24e52bd5c04321c25c3b9
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-13 15:52:34 +0530

    add logging service

---
+++ Test/Stark.CommonTest/LoggingServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Stark.CommonTest
+{
+    [TestClass]
+    public class LoggingServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
